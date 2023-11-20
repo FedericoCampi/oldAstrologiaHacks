b60cfc770b418a631f13295067ce7ef6e6989268
FedericoCampi <federicocampi95@gmail.com>
2023-11-20 16:41:47 +0000

    fonts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d2609eb3ceee0ac"; }
+/******/ 		__webpack_require__.h = function() { return "517c3e9412099779"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
